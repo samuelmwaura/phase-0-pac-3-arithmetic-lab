cac357fa8059022c634555a682bf02a7535d5b54
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-01 23:11:55 +0300

    Completed the assignment.

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(){
+    return 2+5;
+}
+
+
+function subtract(){
+    return 6-3
+}
+
+function divide(){
+    return 7/9;
+}
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+ return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function increment(n){
+    n++;
+    return n;
+}
+
+function decrement(n){
+    n--;
+    return n;
+}
+
+function makeInt(n){
+  return parseInt(n,10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+
+console.log(increment(182));
+console.log(makeInt('3'));
